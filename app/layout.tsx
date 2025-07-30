ea7cd50468986295799078883060c2094d9bc3a7
Seth <srichardson@activecampaign.com>
2025-07-29 20:42:33 -0400

    Add complete PWA setup with icons, splash screens, and native install prompts

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,12 +70,13 @@ export const metadata: Metadata = {
   other: {
     'mobile-web-app-capable': 'yes',
     'apple-mobile-web-app-capable': 'yes',
-    'apple-mobile-web-app-status-bar-style': 'default',
+    'apple-mobile-web-app-status-bar-style': 'black-translucent',
     'apple-mobile-web-app-title': 'SensorySmart',
     'application-name': 'SensorySmart',
-    'msapplication-TileColor': '#F6F6F6',
+    'msapplication-TileColor': '#367A87',
     'msapplication-tap-highlight': 'no',
-    'theme-color': '#F6F6F6',
+    'theme-color': '#367A87',
+    'apple-mobile-web-app-orientations': 'portrait',
   },
 };
 
@@ -106,13 +107,20 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
-        <meta name="theme-color" content="#F6F6F6" />
+        <meta name="theme-color" content="#367A87" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="SensorySmart" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-TileColor" content="#F6F6F6" />
+        <meta name="msapplication-TileColor" content="#367A87" />
         <meta name="msapplication-tap-highlight" content="no" />
+        
+        {/* iOS Splash Screen Images */}
+        <link rel="apple-touch-startup-image" media="screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" href="/splash-screens/apple-touch-startup-image-640x1136.png" />
+        <link rel="apple-touch-startup-image" media="screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" href="/splash-screens/apple-touch-startup-image-750x1334.png" />
+        <link rel="apple-touch-startup-image" media="screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" href="/splash-screens/apple-touch-startup-image-1125x2436.png" />
+        <link rel="apple-touch-startup-image" media="screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)" href="/splash-screens/apple-touch-startup-image-1170x2532.png" />
+        <link rel="apple-touch-startup-image" media="screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)" href="/splash-screens/apple-touch-startup-image-1284x2778.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
